55352838e97a111540435c49e5f7887dca971c0f
Mikhail Lukinov <ecchi.oni.misha@gmail.com>
2023-01-14 20:05:51 -0800

    fifth

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -15,10 +15,8 @@ namespace TestProject2
         [ClassInitialize]
         public static void ClassInitialize(TestContext testContext)
         {
-            // _baseUrl = testContext.Properties["webAppUrl"].ToString();
-            // _numberOfClicks = Int32.Parse(testContext.Properties["numberOfClicks"].ToString());
-            _baseUrl = @"https://the-internet.herokuapp.com/";
-            _numberOfClicks = 6;
+            _baseUrl = testContext.Properties["webAppUrl"].ToString();
+            _numberOfClicks = Int32.Parse(testContext.Properties["numberOfClicks"].ToString());           
         }
         [TestInitialize]
         public void Setup()
